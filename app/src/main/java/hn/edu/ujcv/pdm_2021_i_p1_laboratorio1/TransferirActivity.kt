65e8570bc27b56191577e9fc4b93da51246ed0dd
stephany Carbajal <stephany.carbajal1@ujcv.edu.hn>
2021-02-09 23:41:44 -0600

    Diseños de tres pantallas: tigo,claro y transferir

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p1_laboratorio1/TransferirActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p1_laboratorio1/TransferirActivity.kt
@@ -1,11 +1,18 @@
 package hn.edu.ujcv.pdm_2021_i_p1_laboratorio1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class TransferirActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transferir)
+    }
+    fun menuPrincipal(view: View) {
+        val volverMenu = Intent(this, InicioAppActivity::class.java)
+        startActivity(volverMenu)
+
     }
 }
