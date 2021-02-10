dd74ad3a67ad3e7918abcca60eae72066652042d
stephany Carbajal <stephany.carbajal1@ujcv.edu.hn>
2021-02-09 19:29:21 -0600

    Correcion de error y codigo de cambio de pantalla

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p1_laboratorio1/InicioAppActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p1_laboratorio1/InicioAppActivity.kt
@@ -3,29 +3,45 @@ package hn.edu.ujcv.pdm_2021_i_p1_laboratorio1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_inicio_app.*
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
+
+
 
 class InicioAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_app)
-        //getNombre()
-    }
-    /**fun getNombre(){
-        val bundle = intent.extras
-        val nombre = bundle?.get("nombre")
-        txtvSaludar.text = getString(R.string.bienvenido, nombre)
-    }
-    private fun bootones(){
-        btnENEE.setOnClickListener{compraE()}
-        
-    }
-    private fun compraE(){
-        val intent = Intent(this, ComprarEneeActivity::class.java)
-        startActivity(intent)
-    }
-    private fun compraS(){}**/
 
+
+    }
+    fun abrirEnee(view: View) {
+        val cambio = Intent(this, ComprarEneeActivity::class.java)
+        startActivity(cambio)
+    }
+
+    fun abrirSanaa(view: View) {
+        val cambio = Intent(this, ComprarSanaaActivity::class.java)
+        startActivity(cambio)
+    }
+
+    fun abrirTigo(view: View) {
+        val cambio = Intent(this, RecargaTigoActivity::class.java)
+        startActivity(cambio)
+    }
+
+    fun abrirClaro(view: View) {
+        val cambio = Intent(this, RecargaClaroActivity::class.java)
+        startActivity(cambio)
+    }
+
+    fun abrirRetirar(view: View) {
+        val cambio = Intent(this, RetirarDineroActivity::class.java)
+        startActivity(cambio)
+    }
+
+    fun abrirTransferir(view: View) {
+        val cambio = Intent(this, TransferirActivity::class.java)
+        startActivity(cambio)
+    }
 
 }
