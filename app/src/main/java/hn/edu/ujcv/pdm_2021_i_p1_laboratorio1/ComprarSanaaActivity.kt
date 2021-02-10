8d77283a86c48715d031bc26055ba7c8be1f3241
alycastro22 <allisson.castro@ujcv.edu.hn>
2021-02-10 17:24:24 -0600

    Diseño Pantalla,ENEE,SANAA,RETIRAR

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p1_laboratorio1/ComprarSanaaActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p1_laboratorio1/ComprarSanaaActivity.kt
@@ -1,11 +1,18 @@
 package hn.edu.ujcv.pdm_2021_i_p1_laboratorio1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ComprarSanaaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comprar_sanaa)
+    }
+
+    fun menuPrincipal(view: View) {
+        val regresarMenu = Intent(this, InicioAppActivity::class.java)
+        startActivity(regresarMenu)
     }
 }
--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p1_laboratorio1/RetirarDineroActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p1_laboratorio1/RetirarDineroActivity.kt
@@ -1,11 +1,18 @@
 package hn.edu.ujcv.pdm_2021_i_p1_laboratorio1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class RetirarDineroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_retirar_dinero)
+    }
+
+    fun menuPrincipal(view: View) {
+        val regresarMenu = Intent(this, InicioAppActivity::class.java)
+        startActivity(regresarMenu)
     }
 }
